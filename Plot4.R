3870dc34d6c763e2c749214cc73498f1624bf21d
rbwiley <rb_wiley@aar.com>
2015-03-07 20:30:10 -0700

    final plot versions and plot code

--- Plot4.R
+++ Plot4.R
@@ -37,16 +37,16 @@ plot4 <- function() {
   ##PLOT 1
   plot(pdat$DateTime,pdat$Gl_act_pwr, type="l", xlab="", ylab="Global Active Power")
   ##PLOT 2
-  plot(pdat$DateTime,pdat$Voltage, type="l", xlab="DateTime", ylab="Voltage")
+  plot(pdat$DateTime,pdat$Voltage, type="l", xlab="datetime", ylab="Voltage")
   
   ##PLOT 3
-  plot(pdat$DateTime,pdat$Sub_m1, type="l", xlab="", ylab="Energy Sub Metering")
+  plot(pdat$DateTime,pdat$Sub_m1, type="l", xlab="", ylab="Energy sub metering")
   lines(pdat$DateTime,pdat$Sub_m2,col="red")
   lines(pdat$DateTime,pdat$Sub_m3,col="blue")
-  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5) 
+  legend("topright", col=c("black","red","blue"), c("Sub_metering_1      ","Sub_metering_2      ", "Sub_metering_3      "),lty=c(1,1), bty="c", cex=.4) 
   
   #create plot 4
-  plot(pdat$DateTime,pdat$Gl_react_pwr, type="l", xlab="DateTime", ylab="Global_reactive_power")
+  plot(pdat$DateTime,pdat$Gl_react_pwr, type="l", xlab="datetime", ylab="Global_reactive_power")
   
   # create plot file
   dev.copy(png, file="plot4.png", width=480, height=480)
